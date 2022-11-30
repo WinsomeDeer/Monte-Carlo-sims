68e5c2170f9540c523170c07cc13f3e1a83f6c06
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2022-11-30 23:31:56 +0000

    Update panini.py

--- Panini World Cup sticker simulator./panini.py
+++ Panini World Cup sticker simulator./panini.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import random
 # List to keep track of cards collected.
 cards_collected = np.zeros(830)
+cost = 0.80
 # Function to open the pack.
 def pack_opener():
     pack = random.sample(range(1,830), 5)
