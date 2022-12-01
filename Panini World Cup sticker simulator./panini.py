197c2942d9ed00d673c6a3ddde72d605e7719b87
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2022-12-01 23:29:24 +0000

    Update panini.py

--- Panini World Cup sticker simulator./panini.py
+++ Panini World Cup sticker simulator./panini.py
@@ -1,14 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-# List to keep track of cards collected.
-cards_collected = np.zeros(830)
+# Cost of a single pack.
 cost = 0.80
-# Function to open the pack.
-def pack_opener():
+# Function to open the pack and add new cards to the collection.
+def pack_opener(cards_collected):
     pack = random.sample(range(1,830), 5)
-    return pack
-
-    
-    
-    
+    for i in range(len(pack)):
+        cards_collected[i] += 1
+    return cards_collected
+# Simulate a single attempt to collect all the cards.
+def single_sim_panini():
+    cards_collected = np.zeros(830) # List to keep track of cards collected.
+    while(np.count_nonzero(cards_collected) != 830):
+        collection = pack_opener(cards_collected)
+        amount += cost
+    return amount
