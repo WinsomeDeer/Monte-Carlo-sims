6eab78edbc9cf996ac8a3aff85c4e4fec0c5c6a0
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2022-12-04 17:26:38 +0000

    Update panini.py

--- Panini World Cup sticker simulator./panini.py
+++ Panini World Cup sticker simulator./panini.py
@@ -5,31 +5,29 @@ import random
 cost = 0.80
 # Function to open the pack and add new cards to the collection.
 def pack_opener(cards_collected):
-    pack = random.sample(range(1,830), 5)
+    pack = random.sample(range(0,829), 5)
     for i in range(len(pack)):
         cards_collected[pack[i]] += 1
     return cards_collected
 # Simulate a single attempt to collect all the cards.
-def single_sim_panini():
+def single_sim_panini(single_card_collection):
     number_of_packs = 0
-    collect = []
-    cards_collected = np.zeros(830) # List to keep track of cards collected.
-    while(np.count_nonzero(cards_collected) != 830):
+    while(np.count_nonzero(single_card_collection) != 830):
         number_of_packs += 1
-        collect = pack_opener(cards_collected)
+        single_card_collection = pack_opener(single_card_collection)
     return number_of_packs
 # Simulate n sticker collection attempts.
 def many_sim_panini(n):
-    record_of_packs_opened = [0]*n
+    record_of_packs_opened = np.zeros(830)
     for i in range(n):
-        collect_attempt = []
+        collect_attempt = np.zeros(830)
         pack_attempt = single_sim_panini(collect_attempt)
         record_of_packs_opened[i] = pack_attempt
     return record_of_packs_opened
 # Simulate n sticker collection attempts (cost).
 def many_sim_panini_cost(record_of_packs_opened):
     n = len(record_of_packs_opened)
-    record_of_cost = [0]*n
+    record_of_cost = np.zeros(830)
     for i in range(n):
         record_of_cost[i] = record_of_packs_opened * cost
     return record_of_cost
