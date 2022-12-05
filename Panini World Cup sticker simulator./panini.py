ff7940aa3e37b10251e57928796f7d1094705293
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2022-12-05 00:25:20 +0000

    Update panini.py

--- Panini World Cup sticker simulator./panini.py
+++ Panini World Cup sticker simulator./panini.py
@@ -1,36 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-# Cost of a single pack.
-cost = 0.80
-# Function to open the pack and add new cards to the collection.
-def pack_opener(cards_collected):
-    pack = random.sample(range(0,829), 5)
-    for i in range(len(pack)):
-        cards_collected[pack[i]] += 1
-    return cards_collected
 # Simulate a single attempt to collect all the cards.
 def single_sim_panini(single_card_collection):
-    number_of_packs = 0
-    while(np.count_nonzero(single_card_collection) != 830):
-        number_of_packs += 1
-        single_card_collection = pack_opener(single_card_collection)
-    return number_of_packs
+    for i in range(10000):
+        pack = random.sample(range(0,638), 5)
+        for j in range(5):
+            single_card_collection[pack[j]] = 0
+        if np.sum(single_card_collection) == 0:
+            break
+    return i
 # Simulate n sticker collection attempts.
 def many_sim_panini(n):
-    record_of_packs_opened = np.zeros(830)
+    record_of_packs_opened = np.zeros((n,), dtype = int)
     for i in range(n):
-        collect_attempt = np.zeros(830)
+        collect_attempt = np.ones((638,), dtype = int)
         pack_attempt = single_sim_panini(collect_attempt)
         record_of_packs_opened[i] = pack_attempt
     return record_of_packs_opened
 # Simulate n sticker collection attempts (cost).
 def many_sim_panini_cost(record_of_packs_opened):
     n = len(record_of_packs_opened)
-    record_of_cost = np.zeros(830)
+    cost = 0.80
+    record_of_cost = np.zeros((n,), dtype = int)
     for i in range(n):
-        record_of_cost[i] = record_of_packs_opened * cost
+        record_of_cost[i] = record_of_packs_opened[i] * cost
     return record_of_cost
 # Statistical analysis of 10000 attempts.
 pack_data = many_sim_panini(10000)
 cost_data = many_sim_panini_cost(pack_data)
+# Histograms of the data.
+plt.hist(pack_data, bins = 100)
+plt.hist(cost_data, bins = 100)
+plt.show()
+# Statistical quantities (packs).
+mu_pack = np.mean(pack_data)
+sigma_pack = np.std(pack_data)
+LQ_pack = np.quantile(pack_data, 0.25)
+median_pack = np.median(pack_data)
+UQ_pack = np.quantile(pack_data, 0.75)
+# Statistical quantities (cost).
+mu_cost = np.mean(cost_data)
+sigma_cost = np.std(cost_data)
+LQ_cost = np.quantile(cost_data, 0.25)
+median_cost = np.median(cost_data)
+UQ_cost = np.quantile(cost_data, 0.75)
+print(mu_pack)
+print(mu_cost)
