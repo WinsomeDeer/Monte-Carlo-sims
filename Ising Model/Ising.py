13aac66fb2e51a2be5a2800d54b2437848e0c406
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2022-12-08 19:28:47 +0000

    Update Ising.py

--- Ising Model/Ising.py
+++ Ising Model/Ising.py
@@ -1,1 +1,17 @@
-
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+# Initialise the grid.
+n = 50
+grid = np.ones((n,n), dtype = int) 
+# Boltzman's constant.
+k = 1.38 * 10 ** -27
+# Create a random grid of 1's and -1's.
+for i in range(n):
+    for j in range(n):
+        grid[i][j] = random.choice([-1,1])
+# Function to iterate through the grid and calculate the new grid values.
+def spin_change(x , J, T):
+    for i in range(n):
+        for j in range(n):
+            Q = (J/(k*T)) * (grid[i-1][j] + grid[i+1][j] + grid[i][j-1] + grid[i][j+1])
