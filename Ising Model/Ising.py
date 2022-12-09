515a44c176c3f0a0c23c2c54c29232120cfe0982
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2022-12-09 23:51:49 +0000

    Update Ising.py

--- Ising Model/Ising.py
+++ Ising Model/Ising.py
@@ -10,8 +10,9 @@ k = 1.38 * 10 ** -27
 for i in range(n):
     for j in range(n):
         grid[i][j] = random.choice([-1,1])
-# Function to iterate through the grid and calculate the new grid values.
+# Function to iterate through the grid and calculate the new grid values, where x is the grid, J is magnetic interaction & T is temp.
 def spin_change(x , J, T):
     for i in range(n):
         for j in range(n):
             Q = (J/(k*T)) * (grid[i-1][j] + grid[i+1][j] + grid[i][j-1] + grid[i][j+1])
+            
